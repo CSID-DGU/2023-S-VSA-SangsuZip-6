ea4182c5ec3f7a4c5c8e2b8d63e02c63dd87b27e
w0no <jwh1401@dgu.ac.kr>
2023-07-30 03:29:42 +0900

    feat: 알림 레이아웃 및 배치

--- SangsuZip-Frontend/App.js
+++ SangsuZip-Frontend/App.js
@@ -1,12 +1,34 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import Item from "./components/Item";
+import Header from "./components/Header";
+import SubHeader from "./components/SubHeader";
+import Category from "./components/Category";
+import ConfirmButton from "./components/ConfirmButton";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Header style={styles.header} />
+      <View style={styles.subheader}>
+        <SubHeader />
+        <Category />
+      </View>
+      <View style={styles.body}>
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+      </View>
+      <View style={styles.footer}>
+        <ConfirmButton />
+      </View>
+      {/* <StatusBar style="auto" /> */}
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +36,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+  header: {
+    flex: 1,
+  },
+  subheader: {
+    flex: 2,
+  },
+  body: {
     alignItems: "center",
-    justifyContent: "center",
+  },
+  footer: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 20,
   },
 });
