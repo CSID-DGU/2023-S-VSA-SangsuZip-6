fdda52ab487e8ba18329e1c530be323297fc79cc
w0no <jwh1401@dgu.ac.kr>
2023-08-17 21:29:56 +0900

    feat: expo-notifications를 통한 로컬 푸시 알림 구현

--- SangsuZip-Frontend/pages/AlertPage.jsx
+++ SangsuZip-Frontend/pages/AlertPage.jsx
@@ -1,10 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Header from "../components/Header";
 import SubHeader from "../components/SubHeader";
 import Category from "../components/Category";
 import Item from "../components/Item";
 import ConfirmButton from "../components/ConfirmButton";
 import { SafeAreaView, StyleSheet, View } from "react-native";
+import * as Notifications from "expo-notifications";
+import * as Device from "expo-device";
+import Constants from "expo-constants";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
 
 function AlertPage() {
   const datas = {
@@ -57,13 +68,45 @@ function AlertPage() {
   const [isChecked, setIsChecked] = useState(false);
   const [abnormalDatas, setAbnormalDatas] = useState();
 
+  //
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync()
+      .then((token) => {
+        setExpoPushToken(token);
+      })
+      .then(async () => {
+        await schedulePushNotification();
+      });
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
   useEffect(() => {
     const filteredAbnormalDatas = datas.data.filter(
       (data) => data.isChecked === isChecked
     );
     setAbnormalDatas(filteredAbnormalDatas);
   }, [isChecked]);
-
+  console.log(expoPushToken);
   return (
     <SafeAreaView style={styles.container}>
       <Header style={styles.header} />
@@ -105,5 +148,52 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
 });
+
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "You've got mail! 📬",
+      body: "Here is the notification body",
+      data: { data: "goes here" },
+    },
+    trigger: { seconds: 2 },
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Device.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    const projectId = "c4dc44fd-30e1-4823-9070-77db28045df5";
+    token = await Notifications.getExpoPushTokenAsync({
+      projectId,
+    });
+
+    console.log(token.data);
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  return token.data;
+}
 
 export default AlertPage;
