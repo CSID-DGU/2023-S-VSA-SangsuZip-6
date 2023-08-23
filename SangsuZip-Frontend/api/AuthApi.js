5b561902b51abb572424649be26b9ba17225d89f
w0no <jwh1401@dgu.ac.kr>
2023-08-23 22:16:21 +0900

    feat: 로그인 api 구현

--- SangsuZip-Frontend/api/AuthApi.js
+++ SangsuZip-Frontend/api/AuthApi.js
@@ -1,11 +1,9 @@
 import api from "./index";
+import { SERVER_URL } from "@env";
 
 export const LoginApi = async (data) => {
   try {
-    const response = await api.post(
-      `${process.env.SERVER_URL}/user/signin`,
-      data
-    );
+    const response = await api.post(`${SERVER_URL}/user/signin`, data);
     return response;
   } catch (e) {
     if (e) throw Error(e);
@@ -14,12 +12,16 @@ export const LoginApi = async (data) => {
 
 export const RegisterApi = async (data) => {
   try {
-    const response = await api.post(
-      `${process.env.SERVER_URL}/user/signup`,
-      data
-    );
+    const response = await api.post(`${SERVER_URL}/user/signup`, data);
     return response;
   } catch (e) {
     if (e) throw Error(e);
   }
+};
+
+export const Test = async () => {
+  const response = await api.get(
+    "https://jsonplaceholder.typicode.com/todos/1"
+  );
+  return response;
 };
